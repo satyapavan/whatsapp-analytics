413fe476de9c17756277041f44fb95f429e72790
Satya Pavan <pavan.simham@gmail.com>
2021-06-25 22:47:50 +0530

    initial commit

---
+++ raw_parser.py
@@ -0,0 +1,8 @@
+
+
+""" This should
+1. read the raw whats app file
+2. configure regex to read multi line messages
+3. extract messages based on a threshold message length limit
+4. create a hash to avoid/merge duplicate messages
+"""
